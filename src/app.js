3fe4c776d26c93629fd93c0cffa66674ecd4872a
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-10 13:35:01 +0100

    Add basic code for ping-response bot

--- src/app.js
+++ src/app.js
@@ -1,37 +1,18 @@
-// app.js
-import express from 'express'
-import path from 'path'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
-import createError from 'http-errors'
-import cors from "cors"
+import Discord from 'discord.js'
+import { getClientToken } from './Utils/GetClientToken.js'
 
-import exampleRouter from './routes/example'
+const client = new Discord.Client()
 
-const app = express()
-
-app.use(cors())
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-
-app.use('/example/endpoint', exampleRouter)
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
+client.on('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.send('500 - Internal Server Error')
+client.on('message', async (message) => {
+  if (message.content === 'ping') {
+    message.reply('pong')
+  }
 })
 
-export default app
+getClientToken().then((token) => {
+  client.login(token)
+})
