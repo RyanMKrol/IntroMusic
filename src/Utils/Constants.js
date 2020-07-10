c8c0bc40abaab926792a6ef8f47b2d61a0f5fa90
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-10 15:50:33 +0100

    Abstract JSON file reading to single method

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,2 +1,5 @@
 export const COMMAND_ADD = 'Add'
 export const COMMAND_REMOVE = 'Remove'
+export const DISCORD_TOKEN_FILE_LOCATION = '/credentials/discordToken.json'
+export const DYNAMO_CREDENTIALS_FILE_LOCATION =
+  '/credentials/dynamoCredentials.json'
---
+++ src/Utils/ReadJsonFile.js
@@ -0,0 +1,12 @@
+import util from 'util'
+import path from 'path'
+import fs from 'fs'
+
+const readFile = util.promisify(fs.readFile)
+
+export async function readJsonFile(relativePath) {
+  console.log(path.resolve())
+  return readFile(path.resolve() + relativePath, 'utf8').then((data) => {
+    return JSON.parse(data)
+  })
+}
