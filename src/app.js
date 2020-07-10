53798f3339a4c85f5698dc2d9ee74660dd70c84f
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-10 15:52:54 +0100

    Add basic code for storing/playing intro music when joining voice channel

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import Discord from 'discord.js'
 import ytdl from 'ytdl-core'
-import { getClientToken } from './Utils/GetClientToken.js'
+import { readJsonFile } from './Utils/ReadJsonFile.js'
 import { isUndefined } from './Utils/IsUndefined.js'
-import { COMMAND_ADD, COMMAND_REMOVE } from './Utils/Constants.js'
+import {
+  COMMAND_ADD,
+  COMMAND_REMOVE,
+  DISCORD_TOKEN_FILE_LOCATION,
+} from './Utils/Constants.js'
+import { storeIntroMusic, fetchIntroMusic } from './Storage/Storage.js'
 
 const client = new Discord.Client()
 
@@ -21,13 +26,23 @@ client.on('message', async (message) => {
   const args = message.content.slice(commandPrefix.length).split(' ')
   const command = args.shift()
 
+  if (args.length !== 1) {
+    message.reply(
+      'The command format is incorrect, please use the following format: \n`!intro<Command> <youtube link>`'
+    )
+  }
+
   if (command === COMMAND_ADD) {
+    await storeIntroMusic(message.author.id, args[0])
+    message.reply('Stored your preference!')
     // do something to add
   } else if (command === COMMAND_REMOVE) {
     // do something to remove
   } else {
     message.reply(
-      `This command isn't supported, please use either !intro${COMMAND_ADD}, or !intro${COMMAND_REMOVE}`
+      `This command (\`${commandPrefix}${command}\`) isn't supported, please use one of:\n` +
+        `* \`${commandPrefix}${COMMAND_ADD}\`\n` +
+        `* \`${commandPrefix}${COMMAND_REMOVE}\``
     )
   }
 })
@@ -36,15 +51,22 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
   let oldUserChannel = oldState.channelID
   let newUserChannel = newState.channelID
 
-  if (isUndefined(oldUserChannel) && !isUndefined(newUserChannel)) {
-    console.log(`User ${newState.member} joined a channel!`)
-    // User Joins a voice channel
-  } else if (isUndefined(newUserChannel)) {
-    console.log(`User ${newState.member} left a channel!`)
-    // User leaves a voice channel
+  if (
+    isUndefined(oldUserChannel) &&
+    !isUndefined(newUserChannel) &&
+    !newState.member.user.bot
+  ) {
+    const introMusic = await fetchIntroMusic(newState.member.user.id)
+
+    newState.channel.join().then((connection) => {
+      const stream = ytdl(introMusic.Items[0].link, {
+        filter: 'audioonly',
+      })
+      const dispatcher = connection.play(stream)
+    })
   }
 })
 
-getClientToken().then((token) => {
-  client.login(token)
+readJsonFile(DISCORD_TOKEN_FILE_LOCATION).then((data) => {
+  client.login(data.token)
 })
