f76379b1e2ddebbf04c4503c9426031a15eb32c4
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-10 15:51:53 +0100

    Add methods for fetching/storing/creating dynamo data

---
+++ src/Storage/Storage.js
@@ -0,0 +1,41 @@
+import DynamoDBWrapper from './DynamoClient.js'
+import { readJsonFile } from './../Utils/ReadJsonFile.js'
+import { DYNAMO_CREDENTIALS_FILE_LOCATION } from './../Utils/Constants.js'
+
+let dynamoDb = null
+
+async function fetchDynamoClient() {
+  if (dynamoDb === null) {
+    const dynamoCredentials = await readJsonFile(
+      DYNAMO_CREDENTIALS_FILE_LOCATION
+    )
+
+    dynamoDb = new DynamoDBWrapper(dynamoCredentials, 'us-east-2')
+  }
+
+  return dynamoDb
+}
+
+export async function storeIntroMusic(userId, musicLink) {
+  const client = await fetchDynamoClient()
+
+  const table = 'IntroMusic'
+  const item = {
+    userId: userId,
+    link: musicLink,
+  }
+
+  await client.writeTable(table, item)
+}
+
+export async function fetchIntroMusic(userId) {
+  const client = await fetchDynamoClient()
+
+  const table = 'IntroMusic'
+  const expression = 'userId = :userId'
+  const expressionData = {
+    ':userId': userId,
+  }
+
+  return await client.readTable(table, expression, expressionData)
+}
