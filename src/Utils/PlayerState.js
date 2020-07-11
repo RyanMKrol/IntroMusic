3b5b2124be9e8bcfc4fe255949a4ffb77beefe72
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-11 16:44:54 +0100

    Remove debugging from player state

--- src/Utils/PlayerState.js
+++ src/Utils/PlayerState.js
@@ -3,7 +3,6 @@ let singleton = null
 export default class PlayerState {
   constructor() {
     if (singleton === null) {
-      console.log('setting a thing')
       this.playing = false
       singleton = this
     }
