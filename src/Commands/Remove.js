56fde0685055538ed532310d51f3fe03e4c32991
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-11 16:53:42 +0100

    Move arg validation to remove command

--- src/Commands/Remove.js
+++ src/Commands/Remove.js
@@ -1,11 +1,19 @@
 import { removeIntroMusic } from './../Storage/Storage.js'
 import { doCommandWithRateLimit } from './CommandWrapper.js'
+import { COMMAND_PREFIX, COMMAND_REMOVE } from './../Utils/Constants.js'
 
-export async function remove(messageHook) {
-  const method = async () => {
-    await removeIntroMusic(messageHook.author.id)
-    messageHook.reply('Removed your preference!')
+export async function remove(messageHook, args) {
+  if (args.length !== 0) {
+    await message.reply(
+      `The command format is incorrect, please use the following format: \n\`${COMMAND_PREFIX}${COMMAND_REMOVE}\``
+    )
+    return
   }
 
-  doCommandWithRateLimit(messageHook, method)
+  const method = async () => {
+    await removeIntroMusic(messageHook.author.id)
+    await messageHook.reply('Removed your preference!')
+  }
+
+  await doCommandWithRateLimit(messageHook, method)
 }
