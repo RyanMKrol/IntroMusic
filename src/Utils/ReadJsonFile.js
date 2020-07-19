e52ff7f4feba3270c5f1ad9d02b68fc390b40926
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-19 18:04:15 +0100

    Migrate deployment scripts to noodle-deploy

--- src/Utils/ReadJsonFile.js
+++ src/Utils/ReadJsonFile.js
@@ -5,7 +5,7 @@ import fs from 'fs'
 const readFile = util.promisify(fs.readFile)
 
 export async function readJsonFile(relativePath) {
-  return readFile(path.resolve() + relativePath, 'utf8').then((data) => {
+  return readFile(path.resolve() + relativePath, 'utf8').then(data => {
     return JSON.parse(data)
   })
 }
