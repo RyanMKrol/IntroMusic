de748b9768a163209c0dfb6300f10e52ee2cc760
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-21 17:17:55 +0100

    Add typedef definitions for VoiceState and Guild

--- src/app.js
+++ src/app.js
@@ -7,4 +7,18 @@
  * @see https://discord.js.org/#/docs/main/stable/class/Guild
  */
 
+/**
+ * Type representing the message object from discord.js
+ *
+ * @typedef Message
+ * @see https://discord.js.org/#/docs/main/stable/class/Message
+ */
+
+/**
+ * Type representing the voice state object from discord.js
+ *
+ * @typedef VoiceState
+ * @see https://discord.js.org/#/docs/main/stable/class/VoiceState
+ */
+
 process.stdout.write('Hello World!');
