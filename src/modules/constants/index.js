7107e9eb380c82ed22a2e953c8880f0175e3bf5d
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-21 17:17:02 +0100

    Rename credential files

--- src/modules/constants/index.js
+++ src/modules/constants/index.js
@@ -1,16 +1,20 @@
+// credential constants
+import DISCORD_CREDENTIALS from '../../../credentials/discord.json';
+import DYNAMO_CREDENTIALS from '../../../credentials/dynamo.json';
+
+// command constants
 const COMMAND_PREFIX = '!IM';
 const COMMAND_ADD = 'Add';
 const COMMAND_REMOVE = 'Remove';
 
-const DISCORD_TOKEN_FILE_LOCATION = '/credentials/discordToken.json';
-const DYNAMO_CREDENTIALS_FILE_LOCATION = '/credentials/dynamoCredentials.json';
+// player constants
 const MAX_PLAY_TIME = 12000;
 
 export {
+  DISCORD_CREDENTIALS,
+  DYNAMO_CREDENTIALS,
   COMMAND_PREFIX,
   COMMAND_ADD,
   COMMAND_REMOVE,
-  DISCORD_TOKEN_FILE_LOCATION,
-  DYNAMO_CREDENTIALS_FILE_LOCATION,
   MAX_PLAY_TIME,
 };
