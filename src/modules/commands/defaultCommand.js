18f7b310a24b690a944d25ab3e3cd7770c6ab18f
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-26 21:55:18 +0100

    Add default command
    * will be used when we can't identify the command being
    used

---
+++ src/modules/commands/defaultCommand.js
@@ -0,0 +1,33 @@
+import { COMMAND_PREFIX, COMMAND_REMOVE, COMMAND_ADD } from '../constants';
+
+const IS_BOT_COMMAND_REGEX = `${COMMAND_PREFIX} .*`;
+
+/**
+ * Handles any command not caught by the previous processing
+ *
+ * @param {module:app.Message} messageHook The hook that contains the command being used
+ */
+async function defaultCommand(messageHook) {
+  const command = messageHook.content;
+
+  if (!isBotCommand(command)) return;
+
+  await messageHook.reply(
+    `This command (\`${command}\`) isn't supported, please use one of:\n`
+      + `- \`${COMMAND_PREFIX} ${COMMAND_ADD} <youtube_link>\`\n`
+      + `- \`${COMMAND_PREFIX} ${COMMAND_REMOVE}\``,
+  );
+}
+
+/**
+ * Verifies that the current command belongs to this bot
+ *
+ * @param {string} command The command being used
+ * @returns {boolean} Whether the command belongs to our bot or not
+ */
+function isBotCommand(command) {
+  const isBotCommandPattern = new RegExp(IS_BOT_COMMAND_REGEX);
+  return command.match(isBotCommandPattern);
+}
+
+export default defaultCommand;
--- src/modules/commands/index.js
+++ src/modules/commands/index.js
@@ -1,2 +1,3 @@
 export { default as add } from './add';
 export { default as remove } from './remove';
+export { default as defaultCommand } from './defaultCommand';
