cb98ccf2a773212573e17d571dd77b89440c6579
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-26 21:55:46 +0100

    Add conditional pipeline method to onMessage event handler
    * This will run over a series of functions and stop once
    the command has been processed

--- src/modules/events/onMessage.js
+++ src/modules/events/onMessage.js
@@ -1,5 +1,16 @@
 import { COMMAND_PREFIX } from '../constants';
-import { add, remove } from '../commands';
+import { add, remove, defaultCommand } from '../commands';
+
+/**
+ * A pipeline to run a series of functions until we find a truthy value
+ *
+ * @param {Array.<Function>} fns Array of functions to run over single value
+ * @returns {void} Nothing
+ */
+const conditionalPipeline = (...fns) => (val) => fns.reduce(
+  async (current, fn) => current.then((value) => value || fn(val)),
+  Promise.resolve(false),
+);
 
 /**
  * Method to handle when the bot is messaged by a user
@@ -10,8 +21,9 @@ async function onMessage(message) {
   // ignore any messages not prefixed with our command prefix, or messages made by a bot
   if (!message.content.startsWith(COMMAND_PREFIX) || message.author.bot) return;
 
-  add(message);
-  remove(message);
+  // runs each command handler over the current message, short circuiting once it has
+  // been handled
+  await conditionalPipeline(add, remove, defaultCommand)(message);
 }
 
 export default onMessage;
