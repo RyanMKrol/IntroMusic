928716cd8423a37634cff355755e44094f93ce2c
RyanMKrol <ryankrol.m@gmail.com>
2023-04-01 17:17:35 +0100

    Add validation on link

--- src/modules/commands/add.js
+++ src/modules/commands/add.js
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder, BaseInteraction } from 'discord.js';
+import ytdl from 'ytdl-core';
 
 const SUBCOMMAND_OPTION_NAMES = {
   LINK: 'link',
@@ -24,6 +25,43 @@ export default {
    * @param {BaseInteraction} interaction User interaction object
    */
   async execute(interaction) {
-    interaction.reply('Added!');
+    // the next bit of processing may take time, and discord demands a response within 3s,
+    // deferReply, extends that window for us by displaying something to the user before we
+    // actually reply
+    await interaction.deferReply();
+
+    const link = interaction.options.getString(SUBCOMMAND_OPTION_NAMES.LINK);
+    const video = await parseYouTubeVideoLink(interaction, link);
+
+    if (video) {
+      interaction.followUp('Doing something here!');
+    }
   },
 };
+
+/**
+ * Pull a representation of a video, if the link is valid
+ *
+ * @param {BaseInteraction} interaction User interaction object
+ * @param {string} link A link to validate
+ * @returns {object} Video object from YouTube API
+ * @example Example unavailable video: https://www.youtube.com/watch?v=PE3IslWaB4E
+ */
+async function parseYouTubeVideoLink(interaction, link) {
+  const isLinkToVideo = ytdl.validateURL(link);
+
+  if (!isLinkToVideo) {
+    await interaction.followUp(`Problem with link:\n\`${link}\`\nReason: It doesn't appear to be associated with YouTube`);
+    return undefined;
+  }
+
+  const video = await ytdl.getInfo(link).catch(() => undefined);
+  const isAvailable = video && video.formats.length > 0;
+
+  if (!isAvailable) {
+    await interaction.followUp(`Problem with link:\n\`${link}\`\nReason: It is not available`);
+    return undefined;
+  }
+
+  return video;
+}
